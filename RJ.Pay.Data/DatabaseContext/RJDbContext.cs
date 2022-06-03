a1084f17f0d03a3fae786bf12b64c1f69d47c88e
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2022-06-03 09:07:07 +0430

    Add RJDbContext

---
+++ RJ.Pay.Data/DatabaseContext/RJDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.Pay.Data.DatabaseContext
+{
+    internal class RJDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public RJDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ RJ.Pay.Data/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.Pay.Data.Models
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        public T Id { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        [Required]
+        public DateTime Modified { get; set; }
+    }
+}
