f5b03b446046d4f2300775e265e86d9f1f3d2b96
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2022-09-14 23:53:18 +0430

    sync

---
+++ RJ.Pay.Common/Helpers/ReturnMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.Pay.Common.Helpers
+{
+    public class ReturnMessage
+    {
+        public bool status { get; set; }
+        public string code { get; set; }
+        public string title { get; set; }
+        public string message { get; set; }
+
+    }
+}
